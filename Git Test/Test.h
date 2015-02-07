755174ddd8f4a6c42f0c6103c522883937ffb113
Agi Ogheneogaga Fidelis <aogaga@gmail.com>
2015-02-07 09:38:05 -0700

    added test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Ogaga Agi Fidelis on 2/7/15.
+//  Copyright (c) 2015 Encray Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Ogaga Agi Fidelis on 2/7/15.
+//  Copyright (c) 2015 Encray Inc. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
