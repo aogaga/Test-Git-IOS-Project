4a3bf46ed7f9149cfe5ba2b61702388c1990175d
Ogaga Agi Fidelis <user@Users-MacBook-Pro.local>
2015-02-07 02:57:01 -0700

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Ogaga Agi Fidelis on 2/7/15.
+//  Copyright (c) 2015 Encray Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Git Test
+//
+//  Created by Ogaga Agi Fidelis on 2/7/15.
+//  Copyright (c) 2015 Encray Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
